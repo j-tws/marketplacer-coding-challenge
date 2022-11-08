5055afd24f92e40ac9a4f7ffe65e7cacb7484380
Justin Tan <justintws10@gmail.com>
2022-11-08 21:18:33 +1100

    added json gem to read json file

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 gem "rspec"
 gem 'nokogiri'
+gem 'json'
 gem 'rack', '~> 2.2.4'
---
+++ marketplace.rb
@@ -0,0 +1,43 @@
+# About Marketplacer
+
+# Marketplacer's core product is an e-commerce platform for online marketplaces that connects customers to a range of products to buy. We'd like you to build a checkout system for one of our marketplaces.
+# The Challenge
+
+# Your application should contain the following functionality:
+
+#     Load a list of products from a file.
+#     List product details to the user.
+#     Add products to a Shopping Cart.
+#     Apply promotional discounts.
+#     Calculate and display the total cost.
+
+# You can find the list of available products in the attached file products.json
+
+# List of available promotions is provided below:
+
+#     10% off on total greater than $20
+#     15% off on total greater than $50
+#     20% off on total greater than $100
+
+# The output might but doesn't have to look like the one below:
+
+# Products in Shopping Cart:
+# 1. Jockey Wheels - Orange - $15.39
+# 2. Chain Ring 146mm - $65.95
+# 3. Carbon Brake Pads - $92.00
+# 4. Front Derailleur - 34.9mm - $31.22
+
+# Discount applied: 20% off on total greater than $100
+
+# TOTAL: $163.65
+
+# Happy coding and good luck! 🎉
+# Recommendations
+
+# We encourage you to include a README file with any additional information you would like to provide us with. It's a great place to include the getting started how-to, explain your design decisions etc. We recommend you submit your solution as a CLI application in the language your are most comfortable in, keeping in mind we predominantly use Ruby/Rails, Typescript/React, and a bit of Python. Remember to write a clean and well-structured solution and to commit early and often. Feel free to address the problem creatively and keep the extensibility in mind as we ask successful candidates to extend their implementation.
+
+require 'json'
+
+products_file = File.read('products.json')
+products = JSON.parse(products_file)
+puts products
